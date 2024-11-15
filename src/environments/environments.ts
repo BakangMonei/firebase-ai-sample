37d89e7ec06cef2c57bc4876d3fcd8611a0a4e65
bakangmonei2 <bakangmonei2@gmail.com>
2024-11-15 13:15:24 +0000

    first push

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyBPwHyztHSCEn_PEw3Hy72no38i5uGuWpc',
+      authDomain: 'productdevelopment-a9c31.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'productdevelopment-a9c31',
+      storageBucket: 'productdevelopment-a9c31.firebasestorage.app',
+      messagingSenderId: '612969750246',
+      appId: '1:612969750246:web:57394d83275d2dabbea527',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyB1AyRMGFz8FcblWi47AWC0qsqXDM06ey4',
+  debug_token: ''
+};
